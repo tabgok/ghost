c0746f84868bd1260bde245c7570061334c15f58
tabgok <tabgok@gmail.com>
2025-11-27 19:18:14 -0500

    Shore up the agent some, so that it has all three policies

--- ghost/src/agent/agent.py
+++ ghost/src/agent/agent.py
@@ -1,11 +1,11 @@
 from abc import abstractmethod
-from agent.policies.action.random import RandomPolicy
+from agent.policies.action.policy import ActionPolicy
+from agent.policies.learning.policy import LearningPolicy
+from agent.policies.exploration.policy import ExplorationPolicy
 
 class Agent:
-    def __init__(self, update_policy, action_policy: ActionPolicy, observation_space, action_space) -> None:
-        self.update = update_policy
+    def __init__(self, learning_policy: LearningPolicy, action_policy: ActionPolicy, exploration_policy: ExplorationPolicy, observation_space, action_space) -> None:
+        self.learning = learning_policy
         self.act = action_policy
         self.observation_space = observation_space
         self.action_space = action_space
-
-
